862aa2e886fd27ab03123fb9f556a065294ed1e3
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-27 16:55:50 +0800

    > compile NEMU ysyx_22040000 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:55:50 up  3:02,  1 user,  load average: 0.07, 0.16, 0.22

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,7 +55,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args){
-  printf("single excutaion step!!!");
+  printf("single excutaion step!!!\n");
   return -1;
 }
 
