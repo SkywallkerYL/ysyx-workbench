d7a10da032e38aeaeb0e4b8028e4d65314d2479e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-27 16:44:09 +0800

    > compile NEMU ysyx_22040000 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:44:09 up  2:50,  1 user,  load average: 0.22, 0.30, 0.29

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,6 +54,10 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args){
+  return -1;
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -62,7 +66,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "Single excutaion", cmd_si}
   /* TODO: Add more commands */
 
 };
