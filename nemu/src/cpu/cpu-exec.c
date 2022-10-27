6f938b667e919c994b0c94d487dc9f5d5da25bea
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-27 19:17:15 +0800

    > compile NEMU ysyx_22040000 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:17:15 up 31 min,  1 user,  load average: 0.31, 0.32, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -108,7 +108,7 @@ void cpu_exec(uint64_t n) {
 
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
-  printf("nemu_state :%d/n ",nemu_state.state);
+  printf("nemu_state :%d\n ",nemu_state.state);
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
