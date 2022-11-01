8a13b8f1f7861d972c01994f2a8ac2b261909569
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-01 16:05:15 +0800

    > compile NEMU ysyx_22040000 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:15 up  2:02,  1 user,  load average: 0.48, 0.27, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -294,6 +294,7 @@ int eval (int p , int q) {
   }
   else {
     int op = dominant_operator(p,q);
+    printf("dominator : %d \n",op);
     int val1 = eval (p,op-1);
     int val2 = eval (op+1,q);
     switch (tokens[op].type)
