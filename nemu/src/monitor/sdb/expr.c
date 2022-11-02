9580f9b18b336a9aafcd1f76fb3ff1d8893a6f42
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-02 19:59:44 +0800

    > compile NEMU ysyx_22040000 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:59:44 up 15 min,  1 user,  load average: 0.52, 0.44, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -330,16 +330,16 @@ int eval (int p , int q) {
     int val2 = eval (op+1,q);
     switch (tokens[op].type)
     {
-    case '+' : printf("tempval: %d\n",val1+val2);return val1+val2; break;
-    case '-' : printf("tempval: %d\n",val1-val2);return val1-val2; break;
-    case '*' : printf("tempval: %d\n",val1*val2);return val1*val2; break;
+    case '+' : printf("tempval+: %d\n",val1+val2);return val1+val2; break;
+    case '-' : printf("tempval-: %d\n",val1-val2);return val1-val2; break;
+    case '*' : printf("tempval*: %d\n",val1*val2);return val1*val2; break;
     case '/' : 
       if (val2 == 0)
       {
           assert("Invalid expression for 0 as mother");
       }
       else {
-        printf("tempval: %d\n",val1/val2);
+        printf("tempval/: %d\n",val1/val2);
         return val1/val2; 
       break;}
     case TK_SUB: printf("tempval: %d\n",-val2);return -val2; break; 
