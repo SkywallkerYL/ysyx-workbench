4686f2b69090ee3481a96d0ec53dbd9413443afd
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-03 17:02:28 +0800

    > compile NEMU ysyx_22040000 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:28 up  1:44,  1 user,  load average: 0.30, 0.29, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -83,10 +83,10 @@ void init_regex() {
 
 typedef struct token {
   int type;
-  char str[128];
+  char str[65536];
 } Token;
 
-static Token tokens[128] __attribute__((used)) = {};
+static Token tokens[65536] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
