26cb9af2e304287907a3087e6de4821c58e39ca4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-15 20:54:08 +0800

    > compile NEMU ysyx_22040000 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:54:08 up  1:56,  1 user,  load average: 0.26, 0.15, 0.11

--- nemu/stdout.c
+++ nemu/stdout.c
@@ -5086,7 +5086,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_I: do { *src1 = (cpu.gpr[check_reg_idx(rs1)]); } while (0); do { *imm = ({ struct { int64_t n : 12; } __x = { .n = (((i) >> (20)) & ((1ull << ((31) - (20) + 1)) - 1)) }; (uint64_t)__x.n; }); } while(0); break;
     case TYPE_U: do { *imm = ({ struct { int64_t n : 20; } __x = { .n = (((i) >> (12)) & ((1ull << ((31) - (12) + 1)) - 1)) }; (uint64_t)__x.n; }) << 12; } while(0); break;
     case TYPE_S: do { *src1 = (cpu.gpr[check_reg_idx(rs1)]); } while (0); do { *src2 = (cpu.gpr[check_reg_idx(rs2)]); } while (0); do { *imm = (({ struct { int64_t n : 7; } __x = { .n = (((i) >> (25)) & ((1ull << ((31) - (25) + 1)) - 1)) }; (uint64_t)__x.n; }) << 5) | (((i) >> (7)) & ((1ull << ((11) - (7) + 1)) - 1)); } while(0); break;
-    case TYPE_J: do { *imm = (({ struct { int64_t n : 1; } __x = { .n = (((i) >> (31)) & ((1ull << ((31) - (31) + 1)) - 1)) }; (uint64_t)__x.n; })<<17)|(({ struct { int64_t n : 10; } __x = { .n = (((i) >> (12)) & ((1ull << ((21) - (12) + 1)) - 1)) }; (uint64_t)__x.n; })<<9)| (({ struct { int64_t n : 1; } __x = { .n = (((i) >> (22)) & ((1ull << ((22) - (22) + 1)) - 1)) }; (uint64_t)__x.n; })<< 8) | (((i) >> (23)) & ((1ull << ((30) - (23) + 1)) - 1)); } while(0); break;
+    case TYPE_J: do { *imm = (({ struct { int64_t n : 1; } __x = { .n = (((i) >> (31)) & ((1ull << ((31) - (31) + 1)) - 1)) }; (uint64_t)__x.n; })<<19)|(({ struct { int64_t n : 10; } __x = { .n = (((i) >> (12)) & ((1ull << ((21) - (12) + 1)) - 1)) }; (uint64_t)__x.n; })<<9)| (({ struct { int64_t n : 1; } __x = { .n = (((i) >> (22)) & ((1ull << ((22) - (22) + 1)) - 1)) }; (uint64_t)__x.n; })<< 8) | (((i) >> (23)) & ((1ull << ((30) - (23) + 1)) - 1)); } while(0); break;
   }
 }
 
