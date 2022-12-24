bf2e98ad12f985c22ac037b03494af29fbd91b6d
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-24 19:06:21 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:06:21 up 26 min,  1 user,  load average: 0.22, 0.20, 0.36

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -18,7 +18,7 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if (ref_r->pc!=pc) {printf("i: ref:%08lx nemu:%08lx inpc:%08lx\n",ref_r->pc,cpu.pc,pc);return false;}
+  if (ref_r->pc!=cpu.pc) {printf("i: ref:%08lx nemu:%08lx inpc:%08lx\n",ref_r->pc,cpu.pc,pc);return false;}
   for (size_t i = 0; i < 32; i++)
   {
     if (ref_r->gpr[i]!=cpu.gpr[i])
