adca98a4a9182e25bef251d5b642064729dd4bab
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-24 19:39:49 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:39:49 up 59 min,  1 user,  load average: 0.73, 0.44, 0.37

--- nemu/stdout.c
+++ nemu/stdout.c
@@ -3347,8 +3347,8 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   CPU_state ref_r;
-  printf("pc:%08lx npc:%08lx\n",pc,npc);
-  printf("skip_dut_nr_inst:%d \n",skip_dut_nr_inst);
+
+
   if (skip_dut_nr_inst > 0) {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     printf("ref_r_pc:%08lx \n",ref_r.pc);
@@ -3407,6 +3407,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   ref_difftest_exec(1);
+  printf("ref_r_pc:%08lx \n",ref_r.pc);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
   printf("ref_r_pc:%08lx \n",ref_r.pc);
   checkregs(&ref_r, pc);
