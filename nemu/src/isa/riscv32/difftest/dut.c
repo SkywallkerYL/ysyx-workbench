681a4585b49320a446151118cfd40017b687776b
yangli <577647772@qq.com>
2022-12-25 19:10:35 +0800

    difftest

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,16 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if (ref_r->pc!=pc) return false;
+  for (size_t i = 0; i < 32; i++)
+  {
+    if (ref_r->gpr[i]!=cpu.gpr[i])
+    {
+      return false;
+    }
+    
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
