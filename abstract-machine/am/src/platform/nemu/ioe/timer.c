62eb69f7267bba8572cb24413a73bebdfd47abea
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-26 16:32:55 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:55 up  2:59,  1 user,  load average: 0.24, 0.46, 0.47

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -22,10 +22,10 @@ void __am_timer_init() {
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  rtc->second = 0;
-  rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
+  rtc->second = 1;
+  rtc->minute = 1;
+  rtc->hour   = 1;
+  rtc->day    = 1;
+  rtc->month  = 1;
   rtc->year   = 1900;
 }
