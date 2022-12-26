5f4e7f862af1747379ab22642cd7d74e0639bde5
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-26 16:23:32 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:32 up  2:50,  1 user,  load average: 0.84, 0.63, 0.47

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,14 +6,19 @@
 static uint64_t boot_time = 0;
 
 #define TIME_BASE RTC_ADDR
-
+static uint64_t read_time() {
+  uint32_t lo = *(volatile uint32_t *)(TIME_BASE + 0);
+  uint32_t hi = *(volatile uint32_t *)(TIME_BASE + 4);
+  uint64_t time = ((uint64_t)hi << 32) | lo;
+  return time / 1000000;
+}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;//io_read(AM_TIMER_UPTIME).us;
+  uptime->us = read_time() - boot_time;
 }
 
 void __am_timer_init() {
-  boot_time = 0;
+  boot_time = read_time();
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
