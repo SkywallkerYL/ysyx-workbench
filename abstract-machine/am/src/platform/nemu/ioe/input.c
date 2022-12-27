3caffa88e98396f8baecb65570261d6caedb3489
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-27 13:30:13 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:30:13 up  2:07,  1 user,  load average: 0.27, 0.35, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,31 +1,14 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
+
 //#include </home/yangli/ysyx-workbench/nemu/src/device/keyboard.c>
 #define KEYDOWN_MASK 0x8000
 #define KEYBASE KBD_ADDR
 
-
-
-
-#define KEY_QUEUE_LEN 1024
-static int key_queue[KEY_QUEUE_LEN] = {};
-static int key_f = 0, key_r = 0;
-
-
-static uint32_t key_dequeue() {
-  uint32_t key = AM_KEY_NONE;
-  if (key_f != key_r) {
-    key = key_queue[key_f];
-    key_f = (key_f + 1) % KEY_QUEUE_LEN;
-  }
-  printf("key %x\n",key);
-  return key;
-}
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //uint32_t KEY = *(volatile uint32_t *)(KEYBASE + 0);
   int k = AM_KEY_NONE;
-  k = key_dequeue();
+  k = *(volatile uint32_t *)(KEYBASE + 0);//key_dequeue();
   //uint32_t hi = *(volatile uint32_t *)(KEYBASE + 4);
   kbd->keydown = (k & KEYDOWN_MASK ? true : false);
   kbd->keycode = k & ~KEYDOWN_MASK;
