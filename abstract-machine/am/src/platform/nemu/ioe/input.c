b96e0ee8ed355f30cd4577bb60e51290eaeee674
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-27 11:04:26 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:04:26 up 49 min,  1 user,  load average: 0.58, 0.44, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,7 +3,20 @@
 
 #define KEYDOWN_MASK 0x8000
 #define KEYBASE KBD_ADDR
-
+#define KEY_QUEUE_LEN 1024
+static int key_queue[KEY_QUEUE_LEN] = {};
+static int key_f = 0, key_r = 0;
+enum {
+  _KEY_NONE = 0
+};
+static uint32_t key_dequeue() {
+  uint32_t key = _KEY_NONE;
+  if (key_f != key_r) {
+    key = key_queue[key_f];
+    key_f = (key_f + 1) % KEY_QUEUE_LEN;
+  }
+  return key;
+}
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //uint32_t KEY = *(volatile uint32_t *)(KEYBASE + 0);
   int k = AM_KEY_NONE;
