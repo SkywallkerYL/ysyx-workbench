ace970a53d38345da09805a377903e6a6fbadaff
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-27 18:01:25 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:01:25 up 19 min,  1 user,  load average: 0.42, 0.40, 0.38

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -26,9 +26,20 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .vmemsz = w*h*sizeof(uint32_t)
   };
 }
-
+#include <klib.h>
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   //int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint16_t W = 400;
+  uint16_t H = 300;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t * base = (uint32_t *)ctl->pixels;
+  int cp_bytes = sizeof(uint32_t)* ((w<W-x)?w:W-x);
+  for (int j = 0; j<h&&y+j<H; j++)
+  {
+    memcpy(&fb[(y+j)*W+x],base,cp_bytes);
+    base+=w;
+  }
   
   //if (w == 0 || h == 0) return;
   if (ctl->sync) {
@@ -37,5 +48,6 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
+  //status->ready = true;
+  status->ready = (bool) inl(SYNC_ADDR);
 }
