7548d76c31afa4522b057600ed8c1d1d85e11067
yangli <577647772@qq.com>
2023-01-01 14:31:03 +0800

    riscv-cpu-addi

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdlib.h>
-#include "Vysyx_22050550_cpu.h"
-#include "Vysyx_22050550_cpu__Dpi.h"
+#include "VRiscvCpu.h"
+//#include "VRiscvCpu__Dpi.h"
 #include "verilated_vcd_c.h"
 #include "svdpi.h"
 #define instr_break 0b00000000000100000000000001110011
@@ -12,7 +12,7 @@
 VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
 
-static Vysyx_22050550_cpu* top;
+static VRiscvCpu* top;
 //每一步更新波形
 void step_and_dump_wave(){
   top->eval();
@@ -23,7 +23,7 @@ void step_and_dump_wave(){
 void sim_init(){
   contextp = new VerilatedContext;
   tfp = new VerilatedVcdC;
-  top = new Vysyx_22050550_cpu;
+  top = new VRiscvCpu;
   contextp->traceEverOn(true);
   top->trace(tfp, 0);
   tfp->open("wave.vcd");
@@ -35,23 +35,25 @@ void sim_exit(){
   delete top;
   delete contextp;
 }
+/*
 bool checkebreak ()
 {
-  const svScope scope = svGetScopeFromName("TOP.ysyx_22050550_cpu");
+  const svScope scope = svGetScopeFromName("TOP.RiscvCpu");
   assert(scope);
   svSetScope(scope);
   bool flag = ebreakflag();
   return flag;
 }
+*/
 
 void clockntimes(int n ){
 	
 	int temp = n;
 	while (temp >= 1)
 	{
-		top->clk = 0;
+		top->clock = 0;
 		step_and_dump_wave();
-		top->clk = 1;
+		top->clock = 1;
 		step_and_dump_wave();
 		temp --;
 	}
@@ -60,14 +62,14 @@ int instr_mem[255];
 
 void exuinstr(int pc){
 	int mem_addr = (pc - 0b10000000000000000000000000000000)>>2;
-	
-  top-> clk   = 0;
-  top-> pc    = pc;
-  top-> instr = instr_mem[mem_addr];
+	printf("mem_addr :%d\n",mem_addr);
+  top-> clock   = 0;
+  //top-> pc    = pc;
+  top-> io_instr = instr_mem[mem_addr];
   step_and_dump_wave();
-  top->clk = 1;
+  top->clock = 1;
   step_and_dump_wave();
-  clockntimes(1);
+  //clockntimes(1);
 }
 
 
@@ -79,19 +81,20 @@ int main(int argc , char** argv, char** env) {
   instr_mem[4] = 0b00000000111100001000000100010011;
   instr_mem[5] = 0b00000001111100001000001100010011;
 	sim_init();
-	top->rst = 0b1;
+	top->reset = 0b1;
   clockntimes(4);
-  top->rst = 0b0;
-  top->pc = 0x80000000;
-  clockntimes(10);
+  top->reset = 0b0;
+  //top->pc = 0x80000000;
+  
   int n = 5;
   
   while (n > 0)
   {
-    int pc = top->pc;
+    int pc = top->io_PcRegOut;
+    printf("n %d: pc %x \n",n,pc);
     exuinstr(pc);
-    top->pc = top->npc;
-    if (checkebreak()) break;
+    //top->pc = top->npc;
+    //if (checkebreak()) break;
     n--;
   }
   
