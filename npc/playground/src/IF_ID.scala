0bfa92631c4d18e588c6ecc0124b0fde953ce8ae
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-04 17:06:44 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:06:44 up  3:13,  1 user,  load average: 0.39, 0.44, 0.37

--- npc/playground/src/IF_ID.scala
+++ npc/playground/src/IF_ID.scala
@@ -15,12 +15,14 @@ class IF_ID extends Module{
   })
   //由于中途会有pc跳转，如果这一级是流水线的话，那么会导致后面一条指令被执行，因此
   //这里要根据NOP指令来决定是否刷新
+  
   if(parm.pip){
+
     io.idpc := RegNext(io.ifpc,parm.INITIAL_PC.U(parm.PCWIDTH.W))
     io.idinstr := RegNext(io.ifinstr,0.U(parm.INSTWIDTH.W))
   }
   else {
-    io.idpc := io.ifpc
+    io.idpc := Mux(io.nop,parm.INITIAL_PC.U,io.ifpc)
     io.idinstr := io.ifinstr
   }
 }
