26de0a26360dc390dcc32a8ce39bbbfea33efd9a
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-04 19:28:51 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:28:51 up 41 min,  1 user,  load average: 0.92, 0.63, 0.41

--- npc/playground/src/NPCMUX.scala
+++ npc/playground/src/NPCMUX.scala
@@ -17,10 +17,10 @@ class NPCMUX extends Module{
   val pc_4 = io.PcRegPc + 4.U
   val jalpc  = io.IdPc + io.imm.asUInt
   val jalrpc = (io.imm.asUInt + io.rs1)&(~1.U(parm.REGWIDTH.W))
-  val jumppc = MuxCase(pc_4,Array(
-    (io.jal === 0.U) -> pc_4,
-    (io.jal === 1.U) -> jalpc,
-    (io.jal === 2.U) -> jalrpc
+  val jumppc = MuxLookup(io.jal,pc_4,Seq(
+    0.U -> pc_4   ,
+    1.U -> jalpc  ,
+    2.U -> jalrpc
   ))
   io.NOP := io.jal=/=0.U
   io.NPC := Mux(io.NOP,jumppc,pc_4)
