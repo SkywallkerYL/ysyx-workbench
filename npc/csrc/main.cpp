4ec94a1acdc41643bad7e2b089b850fb445e51b1
yangli <577647772@qq.com>
2023-01-04 20:46:48 +0800

    npc dummy

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -122,8 +122,7 @@ int main(int argc , char* argv[]) {
   reset(5);
   //top->pc = 0x80000000;
   
-  int n = 20 ;
-  while (!top->io_halt&&n--)
+  while (!top->io_halt)
   {
     //printf("pc: %lx\n",top->rootp->RiscvCpu__DOT__PcReg__DOT__reg_0);
     clockntimes(1);
