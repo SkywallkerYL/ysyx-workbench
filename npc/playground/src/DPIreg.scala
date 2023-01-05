df1c4392eac6418b663c06971947b0a498b5ce9f
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-05 21:29:31 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:31 up  3:13,  1 user,  load average: 0.87, 0.81, 0.61

--- npc/playground/src/DPIreg.scala
+++ npc/playground/src/DPIreg.scala
@@ -24,7 +24,7 @@ class regDPI extends BlackBox with HasBlackBoxInline{
     |
     |assign b = a;
     |
-    |import "DPI-C" function void set_gpr_ptr(input logic a);
+    |import "DPI-C" function void set_gpr_ptr(input logic [63:0] a);
     |
     |initial set_gpr_ptr(b);
     |
