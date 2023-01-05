c6b7d0bfad3a433f0f28612c9181655006571c6a
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-05 20:27:38 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:27:38 up  2:12,  1 user,  load average: 0.65, 0.31, 0.24

--- npc/playground/src/REGFILE.scala
+++ npc/playground/src/REGFILE.scala
@@ -19,7 +19,7 @@ class RegFile extends Module{
   })
   val reg = RegInit(VecInit(Seq.fill(parm.RegNumber)(0.U(parm.REGWIDTH.W))))
   val regdpi = Module(new regDPI)
-  regdpi = reg()
+  regdpi.a := reg(1)
   when (io.wen){
     when (io.waddr =/= 0.U) {reg(io.waddr) := io.wdata}
   }
