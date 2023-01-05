2dbb6282be764fd8472234915a8454e32cab86f3
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-05 19:29:29 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:29:29 up  1:13,  1 user,  load average: 0.06, 0.29, 0.34

--- npc/csrc/npcsdb.h
+++ npc/csrc/npcsdb.h
@@ -1,7 +1,7 @@
 #ifndef NPCSDB
 #define NPCSDB
-//#include <readline/readline.h>
-//#include <readline/history.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
@@ -10,12 +10,29 @@
 using namespace std;
 
 
+static char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(nemu) ");
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
+
 string mygetline(){
     //printf("in mygetline\n");
     string line ;
-    getline(cin,line);
+    getline(cin,line,'\n');
+    //cin.getline(str);
     //printf("line_length:%ld\n",line.length());
-
     return line;
 }
 /*
@@ -63,6 +80,27 @@ static int cmd_c(char *args) {
   return 0;
 }
 
+//从起始地址打印给定个数的地址存储的信息
+//example x 10 0x8000_0000
+static int cmd_x(char *args){
+  char *N = strtok(NULL," ");
+  char *EXPR = strtok(NULL," ");
+  int addrn ;
+  sscanf(N,"%d",&addrn);
+  uint64_t addexpr;
+  sscanf(EXPR,"%lx",&addexpr);
+  //printf ("%x\t",addexpr);
+  int init = (addexpr-0x80000000)/4;
+  uint* p = &top->rootp->RiscvCpu__DOT__M[init];
+  for (size_t i = 0; i < addrn; i++)
+  {
+    printf("0x%08lx\t 0x%08x\n",addexpr,*p);
+    addexpr+=4;
+    p++;
+  }
+  printf("\n");
+  return 0;
+}
 
 static struct {
   const char *name;
@@ -72,9 +110,9 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   //{ "q", "Exit NEMU", cmd_q },
-  { "si", "Single excutaion", cmd_si}
-  //{"info","info SUBCMD",cmd_info}
-  //{"x","EXPR SCAN",cmd_x},
+  { "si", "Single excutaion", cmd_si},
+  //{"info","info SUBCMD",cmd_info},
+  {"x","EXPR SCAN",cmd_x}
   //{"p","Expression calculation",cmd_p},
   //{"pt","Expression calculation test",cmd_pt},
   //{"w","Watchpoint add",cmd_w},
@@ -121,13 +159,18 @@ void sdb_mainloop() {
     return;
   }
   //printf("hhhh\n");
+  string quit = "quit";
+  //string enter = '\n';
+  //for (char * str = mygetline(str);str!=NULL;){
   for (string strcin=mygetline(); strcin.length()!= 0; ) {
-   // printf("hjhhhh:%d\n",strcin.length());
+  //for (char *str; (str = rl_gets()) != NULL; ) {
+    //printf("start:%ld\n",strcin.length());
+    //cout<<strcin;
     char str[20];
     strcpy(str,strcin.c_str());
-   // printf("hjhhhh\n");
+    //printf("hjhhhh\n");
     char *str_end = str + strlen(str);
-   // printf("str:%s\n",str);
+    //printf("str:%s\n",str);
     /* extract the first token as the command */
     char *cmd = strtok(str, " ");
     if (cmd == NULL) { continue; }
@@ -154,7 +197,11 @@ void sdb_mainloop() {
     }
 
     if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    //cin.ignore();
+    //acin.get();
     strcin=mygetline();
+    //cout<<strcin;
+    //printf("end:%d\n",strcin.length());
   }
 }
 
