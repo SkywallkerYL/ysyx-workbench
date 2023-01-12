6f08d971553dee3587b1f0ef6e29b1c66190eaa2
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-12 18:54:26 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:54:26 up  4:48,  1 user,  load average: 0.37, 0.50, 0.49

--- npc/csrc/difftest.h
+++ npc/csrc/difftest.h
@@ -112,13 +112,14 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   //Log("nemu memcpy init");
   ref_difftest_regcpy(&refcpu, DIFFTEST_TO_REF);
 }
-
+void assert_fail_msg();
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
     npc_state.state = NPC_ABORT;
     npc_state.halt_pc = pc;
     //printf("Not same!!!\n");
-    isa_reg_display();
+    assert_fail_msg();
+    //isa_reg_display();
   }
 }
 
