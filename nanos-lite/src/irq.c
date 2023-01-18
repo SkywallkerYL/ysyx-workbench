9671bedbb5b89918c3fa0c252386f52b40be8ccd
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 17:58:30 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:58:30 up  4:14,  1 user,  load average: 0.93, 0.50, 0.36

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 1 : printf("Event Yeild\n");//EVENT_YIELD
+    case EVENT_YIELD : printf("Event Yeild\n");break;//EVENT_YIELD
     default: panic("Unhandled event ID = %d", e.event);
   }
 
