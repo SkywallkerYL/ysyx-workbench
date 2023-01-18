666043040e8b7dfa044220d483bb9f47380c5806
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 15:21:25 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:21:25 up  1:37,  1 user,  load average: 0.92, 0.54, 0.36

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -24,7 +24,7 @@ const char *regs0[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-
+//#define CHECK_CSR
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if (ref_r->pc!=cpu.pc) {printf("refpc:%08lx nemupc:%08lx lastpc:%08lx\n",ref_r->pc,cpu.pc,pc);return false;}
   bool regflag = true;
@@ -38,6 +38,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       regflag = false;
     }
   }
+#ifdef CHECK_CSR
   if (ref_r->mepc !=cpu.mepc){
       printf("mepc ref:%08lx nemu:%08lx lastpc:%08lx\n",ref_r->mepc,cpu.mepc,pc);
       regflag = false;
@@ -54,6 +55,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       printf("mstatus ref:%08lx nemu:%08lx lastpc:%08lx\n",ref_r->mstatus,cpu.mstatus,pc);
       regflag = false;
   }
+#endif
   return regflag;
 }
 
