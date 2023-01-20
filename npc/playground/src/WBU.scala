dde119767a5f0ce1a91ac5dfe22aef26d3b1063c
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 18:57:18 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:57:18 up  1:42,  1 user,  load average: 1.11, 0.79, 0.47

--- npc/playground/src/WBU.scala
+++ npc/playground/src/WBU.scala
@@ -56,10 +56,10 @@ class WBU extends Module{
       //io.CsrRegfile.
       mstatus := func.MretMstatus(io.CsrIn.mstatus)
     }
-    io.CsrRegfile.mepc := Mux(io.id.CsrWb_i.CsrExuChoose(0),io.AluRes_i,io.id.CsrWb.CSR.mepc)
-    io.CsrRegfile.mcause := Mux(io.id.CsrWb_i.CsrExuChoose(1),io.AluRes_i,io.id.CsrWb.CSR.mcause)
-    io.CsrRegfile.mtvec := Mux(io.id.CsrWb_i.CsrExuChoose(2),io.AluRes_i,io.id.CsrWb.CSR.mtvec)
-    io.CsrRegfile.mstatus := Mux(io.id.CsrWb_i.CsrExuChoose(3),io.AluRes_i,io.id.CsrWb.CSR.mstatus)
+    io.CsrRegfile.mepc := Mux(io.CsrWb_i.CsrExuChoose(0),io.AluRes_i,io.id.CsrWb.CSR.mepc)
+    io.CsrRegfile.mcause := Mux(io.CsrWb_i.CsrExuChoose(1),io.AluRes_i,io.id.CsrWb.CSR.mcause)
+    io.CsrRegfile.mtvec := Mux(io.CsrWb_i.CsrExuChoose(2),io.AluRes_i,io.id.CsrWb.CSR.mtvec)
+    io.CsrRegfile.mstatus := Mux(io.CsrWb_i.CsrExuChoose(3),io.AluRes_i,io.id.CsrWb.CSR.mstatus)
 
     //io.LsuRes_o :=  io.LsuRes_i  
 }
