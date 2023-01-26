b2c13c4305f44eb4efab3caf4beed9375664efc3
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-26 23:16:58 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:16:58 up  1:45,  1 user,  load average: 0.54, 0.56, 0.37

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,9 +8,26 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
+#if defined(__ISA_AM_NATIVE__) || defined(__ISA_X86_64__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE panic("Unknown type")
+#elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_TYPE panic("Unknown type")
+#else
+# error Unsupported ISA
+#endif
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
+#define MagicNumber  0x7f454c46020101000000000000000000
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  //TODO();
+  Elf_Ehdr elf_head;
+  ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)elf_head.e_ident ==  0x7f454c46);
+
   return 0;
 }
 
