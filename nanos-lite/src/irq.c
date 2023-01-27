bd2362bc9bceaf8e01b346c544a76b7cf6eb8bab
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 18:08:48 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:08:48 up 50 min,  1 user,  load average: 0.62, 0.54, 0.32

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,10 +1,11 @@
 #include <common.h>
 
 //事件分发
+void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case  EVENT_YIELD: printf("Event Yeild\n");break;
-    case  EVENT_SYSCALL : ;break;
+    case  EVENT_SYSCALL :do_syscall(c) ;break;
     case  EVENT_IRQ_TIMER :printf("Event IRQ TIMER\n");break;
     //here a break will build a mret inst
     // but I can't implement it accurately
