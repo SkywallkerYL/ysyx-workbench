8bff32fe6daa6b757ac882ff084b777e18a8bde0
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 14:00:08 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:00:08 up 53 min,  1 user,  load average: 0.76, 0.46, 0.33

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -53,9 +53,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
+  //uintptr_t entry = 
+  loader(pcb, filename);
   //Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  //((void(*)())entry) ();
   //printf("aaaa\n");
 }
 
