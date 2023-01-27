6ceb36925c7ba1929626edea4a135f34c232f1db
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 18:06:20 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:06:20 up 48 min,  1 user,  load average: 0.28, 0.46, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,9 +3,10 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  uint64_t ret ;
   switch (a[0]) {
-    case SYS_yield :yield();break;
+    case SYS_yield :ret = sys_yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  c->GPRx = ret;
 }
