9126219772298a3dac3021cad55459a43d97fd44
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 12:36:34 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:36:34 up  1:41,  1 user,  load average: 0.26, 0.35, 0.35

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,23 +10,26 @@ void do_syscall(Context *c) {
   //GPR2 是a0  存的是程序退出时的状态
   switch (a[0]) {
     case SYS_exit  :
-      sys_exit(c->GPR2);
-#ifdef STRACE
       Log("SYSTEM_CALL_EXIT VALUE%d",c->GPR2);
-#endif
+      sys_exit(c->GPR2);break;
+//#ifdef STRACE
+  //    Log("SYSTEM_CALL_EXIT VALUE%d",c->GPR2);
+//#endif
       //Log("SYSTEM_CALL_EXIT VALUE%d",c->GPR2);
       break;
     case SYS_yield :
-#ifdef STRACE
       Log("SYSTEM_CALL_YIELD");
-#endif
+      ret = sys_yield();break;
+//#ifdef STRACE
+  //    Log("SYSTEM_CALL_YIELD");
+//#endif
       ret = sys_yield();break;
     case SYS_write :
       ret = sys_write(c->GPR2,(void *)c->GPR3,(size_t)c->GPR4);
-#ifdef STRACE
+//#ifdef STRACE
       //ret = sys_write(c->GPR2,(void *)c->GPR3,(size_t)c->GPR4);
       Log("SYSTEM_CALL_WRITE RETURN %d",ret);
-#endif
+//#endif
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
