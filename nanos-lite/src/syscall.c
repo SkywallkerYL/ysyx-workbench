622998e73147fde398e1deefd153833d4897e0c2
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 16:05:24 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:24 up  5:10,  1 user,  load average: 1.13, 0.44, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -44,7 +44,7 @@ void do_syscall(Context *c) {
     case SYS_write :
       ret = sys_write(c->GPR2, (void *)c->GPR3, (size_t)c->GPR4);
 #ifdef STRACE
-      Log("SYSTEM_CALL_WRITE RETURNVALUE%d",ret);
+      Log("SYSTEM_CALL_WRITE RETURNVALUE %d",ret);
 #endif
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
