8359b83849797f61e428f9726286aea93a9ab683
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 13:05:17 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:05:17 up  2:10,  1 user,  load average: 0.51, 0.44, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,8 +10,9 @@ void do_syscall(Context *c) {
   //GPR2 是a0  存的是程序退出时的状态
   switch (a[0]) {
     case SYS_exit  :
+      sys_exit(c->GPR2);
       Log("SYSTEM_CALL_EXIT VALUE %d",c->GPR2);
-      sys_exit(c->GPR2);break;
+      break;
     case SYS_yield :
       Log("SYSTEM_CALL_YIELD");
       ret = sys_yield();break;
