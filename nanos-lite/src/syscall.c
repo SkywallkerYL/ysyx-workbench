0c6f973f1d6d80dbaa33913263d9d3b37177b7af
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 14:46:02 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:02 up  3:51,  1 user,  load average: 0.11, 0.17, 0.16

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,8 +4,8 @@ void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//a7 存mcause
   uint64_t ret ;
-  Log("mcause:%d",a[0]);
-  //GPR2 是a0  存的是程序退出时的状态
+  Log("mcause:%d",c->mcause);
+  //GPR2 是a0,不是这里的a[0]  存的是程序退出时的状态
   switch (a[0]) {
     case SYS_exit  :
       Log("SYSTEM_CALL_EXIT VALUE:%d",c->GPR2);
