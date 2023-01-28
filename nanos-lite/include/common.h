407add100231770efd7716e2c5042d5f2b964e56
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 12:05:42 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:05:42 up  1:10,  1 user,  load average: 0.32, 0.25, 0.20

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -7,6 +7,8 @@
 //#define MULTIPROGRAM
 //#define TIME_SHARING
 
+//#define STRACE
+
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,29 @@
 #include <common.h>
 #include "syscall.h"
+
+
+int sys_yield() {
+    yield();
+    return 0;
+}
+
+void sys_exit(int status) {
+    halt(status);
+}
+//看/home/yangli/ysyx-workbench/nemu/tools/spike-diff/repo/fesvr/syscall.cc
+//sys_write(reg_t fd, reg_t pbuf, reg_t len, reg_t a3, reg_t a4, reg_t a5, reg_t a6)
+//推测fd pbuf len 依次是a0 a1 a2,即GPR2,GPR3,GPR4
+int sys_write(int fd, void *buf, size_t count){
+  if(fd == 1 || fd ==2){
+    for (size_t i = 0; i < count; i++)
+    {
+      putch(*((char*)buf+i));
+    }
+    return count;
+  }
+  return -1;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//a7 存mcause
@@ -7,11 +31,21 @@ void do_syscall(Context *c) {
   //GPR2 是a0  存的是程序退出时的状态
   switch (a[0]) {
     case SYS_exit  :
-      Log("SYSTEM_CALL_EXIT");
       sys_exit(c->GPR2);break;
+#ifdef STRACE
+      Log("SYSTEM_CALL_EXIT VALUE%d",c->GPR2);
+#endif
     case SYS_yield :
+#ifdef STRACE
       Log("SYSTEM_CALL_YIELD");
+#endif
       ret = sys_yield();break;
+    case SYS_write :
+      ret = sys_write(c->GPR2,(void *)c->GPR3,(size_t)c->GPR4);
+#ifdef STRACE
+      //ret = sys_write(c->GPR2,(void *)c->GPR3,(size_t)c->GPR4);
+      Log("SYSTEM_CALL_WRITE RETURN %d",ret);
+#endif
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   c->GPRx = ret;
