4515672066788f2acc5bcb860046547a62a0e4b4
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 17:20:37 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:20:37 up  7:28,  1 user,  load average: 0.56, 0.43, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -52,8 +52,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //elf_head.e_phoff 记录了program的偏移，后边的实现都是参考trace.h
   Elf_Phdr *elf_phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
   //size_t prooffset = elf_head.e_phoff;
-  //assert(fs_lseek(fd, elf_head.e_phoff, SEEK_SET) >= 0);
-  //assert(fs_read(fd,elf_phdr,sizeof(Elf_Phdr) * elf_head.e_phnum) >= 0);
+  assert(fs_lseek(fd, elf_head.e_phoff, SEEK_SET) >= 0);
+  assert(fs_read(fd,elf_phdr,sizeof(Elf_Phdr) * elf_head.e_phnum) >= 0);
   ramdisk_read(elf_phdr, elf_head.e_phoff, sizeof(Elf_Phdr) * elf_head.e_phnum);
   for (size_t i = 0; i < elf_head.e_phnum; i++)
   {
@@ -63,9 +63,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     char * buf_malloc = (char *)malloc(elf_phdr[i].p_filesz * sizeof(char) + 1);
     //printf("offset:%08x\n",elf_phdr[i].p_offset);
     //修改之后不直接用ramdisk_read,而是用fs_read();
-    //assert(fs_lseek(fd, elf_phdr[i].p_offset, SEEK_SET) >= 0);
-    //assert(fs_read(fd,buf_malloc,elf_phdr[i].p_filesz ) >= 0);
-    ramdisk_read(buf_malloc, elf_phdr[i].p_offset,  elf_phdr[i].p_filesz );
+    assert(fs_lseek(fd, elf_phdr[i].p_offset, SEEK_SET) >= 0);
+    assert(fs_read(fd,buf_malloc,elf_phdr[i].p_filesz ) >= 0);
+    //ramdisk_read(buf_malloc, elf_phdr[i].p_offset,  elf_phdr[i].p_filesz );
     memcpy((void *)elf_phdr[i].p_vaddr, buf_malloc, elf_phdr[i].p_filesz );
     memset((void *)(elf_phdr[i].p_vaddr + elf_phdr[i].p_filesz), 0, elf_phdr[i].p_memsz - elf_phdr[i].p_filesz);
     free(buf_malloc);
@@ -73,7 +73,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   assert(fs_close(fd) == 0);
   //printf("hhhhhh\n");
-  return fileoffset;
+  return elf_head.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
