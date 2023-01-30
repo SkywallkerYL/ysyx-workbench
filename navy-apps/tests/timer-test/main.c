3107aacc9bb19b45bb1491eb7086a8864e5ad781
yangli <577647772@qq.com>
2023-01-30 11:50:23 +0800

    time

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -10,7 +10,6 @@ int main(){
     time_t old_sec = init_time.tv_sec;
     suseconds_t old_usec = init_time.tv_sec;
 //
-    int multy = 2;
     int time = 0;
 
     while (1)
@@ -19,11 +18,11 @@ int main(){
         time_t now_sec = now.tv_sec;
         suseconds_t now_usec = now.tv_usec;
         __uint64_t gap_us =  (now_sec-old_sec)* 1000000 + (now_usec-old_usec);
-        if (gap_us>500000*multy)
+        if (gap_us>500000)
         {
             old_sec = now_sec;
             old_usec = now_usec;
-            printf("time:%d %d*0.5s passed\n",time,multy);
+            printf("time:%d 0.5s passed\n",time);
             time++;
         }
         
