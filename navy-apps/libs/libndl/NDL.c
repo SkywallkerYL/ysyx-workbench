f60a471b22bd2a37547ee8c60704f731dede1e4f
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-30 23:34:25 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:34:25 up 10:32,  1 user,  load average: 0.38, 0.54, 0.49

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -80,7 +80,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   {
     uint64_t offset = (y+Canvas_y+i)*screen_w+(x+Canvas_x);
     //printf("offset %d screen_w %d\n",offset,screen_w);
-    printf("x:%d y%d\n",offset/screen_w,offset%screen_w);
+    printf("x:%d y:%d\n",offset%screen_w,offset/screen_w);
     //x+w不能超过画布
     int len = (w+x<Canvas_w)?w:Canvas_w-x;
     lseek(fd,offset*4,SEEK_SET);
