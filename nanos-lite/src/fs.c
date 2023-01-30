e8391f2b2697b6c21da72de8071ad3010362f363
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-30 15:46:02 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:46:02 up  2:44,  1 user,  load average: 1.17, 0.81, 0.54

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -84,7 +84,7 @@ int fs_open(const char *pathname, int flags, int mode)
       //if (i == FD_STDIN || i == FD_STDOUT || i == FD_STDERR)
       if (i < FD_FB)
       {
-        Log("File open ignore %s", pathname);
+        //Log("File open ignore %s", pathname);
         return i;
       }
       OpenFileTable[OpenNum].fd = i;
@@ -191,7 +191,7 @@ size_t fs_lseek(int fd, size_t offset, int whence)
   //if (fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR)
   if (fd < FD_FB)
   {
-    Log("File lseek ignore %s", file_table[fd].name);
+    //Log("File lseek ignore %s", file_table[fd].name);
     return 0;
   }
   int openind = GetOpenInd(fd);
@@ -228,7 +228,7 @@ int fs_close(int fd)
   //if (fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR)
   if (fd < FD_FB)
   {
-    Log("File close ignore %s", file_table[fd].name);
+    //Log("File close ignore %s", file_table[fd].name);
     return 0;
   }
   int openind = GetOpenInd(fd);
