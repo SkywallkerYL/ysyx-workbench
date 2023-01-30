cf990ad28e62aa481221a5df8e3db8dbced8a95f
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-30 22:28:47 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:28:47 up  9:27,  1 user,  load average: 0.27, 0.26, 0.31

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -55,8 +55,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   uint32_t width = ev.width;
   int y = offset/width;
   int x = offset-y*width;
-  int wri_width = len/4;
-  assert(wri_width%4==0);
+  //int wri_width = len/4;
+  //assert(wri_width%4==0);
   //io_write(AM_GPU_FBDRAW, x, y, (void *)buf, width, height, true);
   //传进来的是一行的像素
   io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
