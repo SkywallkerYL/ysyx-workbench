d61e56fa71ad12189daa2bfa024da657320404f9
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-31 17:21:55 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:55 up  4:27,  1 user,  load average: 0.70, 0.91, 0.83

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -34,12 +34,12 @@ int SDL_PollEvent(SDL_Event *ev) {
       //printf("%s\n",keyname[i]);
       if (strncmp(keybuf, keyname[i],len)==0)
       {
-        printf("%s %s\n",keybuf,keyname[i]);
+        //printf("%s %s\n",keybuf,keyname[i]);
         ev->key.keysym.sym = i;
         break;
       }
     }
-    printf("key %s\n",keyname[ev->key.keysym.sym]);
+    //printf("key %s\n",keyname[ev->key.keysym.sym]);
   }
   else return 1;
   return 0;
