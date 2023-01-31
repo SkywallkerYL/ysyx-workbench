ab73ff6404ae71228204aa209f374dc5c7e3cae8
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-31 16:43:52 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:43:52 up  3:49,  1 user,  load average: 0.56, 0.47, 0.43

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,6 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
-
+#include <string.h>
 #define keyname(k) #k,
 
 static const char *keyname[] = {
@@ -13,10 +13,27 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
+  char buf[64];
+  char* keybuf = buf;
+  keybuf+=3;
+  if (NDL_PollEvent(buf, sizeof(buf))) {
+    if(strncmp(buf,"kd",2)==0) {ev->type =SDL_KEYDOWN ;}
+    else if(strncmp(buf,"kb",2)==0){ev->type =SDL_KEYUP ;}
+    else return 1;
+    for (size_t i = 0; i < 83; i++)
+    {
+      if (strcmp(keybuf, keyname[i])==0)
+      {
+        ev->key.keysym.sym = i;
+        break;
+      }
+    }
+  }
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  return SDL_PollEvent(event);
   return 1;
 }
 
