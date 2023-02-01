1cea6b6cbc9289893fa6293b9c55aa1cdfb2247f
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-01 14:09:48 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:09:48 up  4:25,  1 user,  load average: 0.32, 0.40, 0.30

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void init_proc() {
   //loader(NULL,NULL);
   // /bin/hello /bin/file-test /bin/timer-test /bin/event-test
   // /bin/bmp-test /bin/nslider /bin/menu nterm
-  const char filename []= "/bin/nterm";
+  const char filename []= "/bin/bird";
   naive_uload(NULL,filename);
   //printf("aaaa\n");
 }
--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -10,9 +10,29 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(freesrc == 0);
   return NULL;
 }
-
+/*
+    用libc中的文件操作打开文件, 并获取文件大小size
+    申请一段大小为size的内存区间buf
+    将整个文件读取到buf中
+    将buf和size作为参数, 调用STBIMG_LoadFromMemory(), 它会返回一个SDL_Surface结构的指针
+    关闭文件, 释放申请的内存
+    返回SDL_Surface结构指针
+*/
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *fp = fopen(filename,"r");
+  if(fp == NULL) assert(0);
+  fseek(fp,0,SEEK_END);
+  size_t size = ftell(fp);
+  char * buf = (char*)malloc(size*sizeof(char));
+  fseek(fp, 0, SEEK_SET);
+  size_t ret = fread(buf, 1, size, fp);
+  assert(ret == size);
+  SDL_Surface *s = STBIMG_LoadFromMemory(buf,size);
+  assert(s!=NULL);
+  fclose(fp);
+  free(buf);
+  return s;
+  //return NULL;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
