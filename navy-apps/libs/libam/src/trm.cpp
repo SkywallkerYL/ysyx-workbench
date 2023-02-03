04718dba4b1086925c9e7be47197f8bba9b2eb50
yangli <577647772@qq.com>
2023-02-03 12:21:25 +0800

    navy

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,14 +1,9 @@
 #include <am.h>
-# define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+
 Area heap;
 
 void putch(char ch) {
-    putchar(ch);
 }
 
 void halt(int code) {
-    nemu_trap(code);
-
-  // should not reach here
-    while (1);
 }
