c291042bdb0685f5926dc43a5e3e579d514bdad2
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-03 11:59:21 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:59:21 up  2:36,  1 user,  load average: 0.83, 0.48, 0.40

--- navy-apps/libs/libam/include/navy.h
+++ navy-apps/libs/libam/include/navy.h
@@ -2,5 +2,12 @@
 #define __NAVY_H__
 
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+//#include <SDL2.h>
+//#include <NDL.h>
 #endif
