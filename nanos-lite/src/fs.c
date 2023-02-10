09d29f68f6bb380081e921e4b7772d68ab09ab57
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-10 14:14:56 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:14:56 up  3:11,  1 user,  load average: 0.75, 0.59, 0.45

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -87,7 +87,7 @@ int fs_open(const char *pathname, int flags, int mode)
   //printf("%d\n",LENGTH(file_table));
   for (size_t i = 0; i < LENGTH(file_table); i++)
   {
-    printf("%s %s %d\n",pathname,file_table[i].name,file_table[i].size);
+    //printf("%s %s %d\n",pathname,file_table[i].name,file_table[i].size);
     //if(i>=6) printf("%s %s\n",pathname,file_table[i].name);
     if (strcmp(pathname, file_table[i].name) == 0)
     {
