dadc01776d6c6a9aa64581e469302bfc6868483b
tracer-nju <tracer@nju.edu.cn>
2023-02-13 13:18:12 +0800

    Static hostname: yangli-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 4fa1b7e0ffde4777b3cf6353843f97b8          Boot ID: 04371be7fc5a49e3854dc735afe7f29c   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  13:18:12 up  3:21,  1 user,  load average: 0.32, 0.28, 0.19

--- ics-workbench-lab4/cachesim/cache.c
+++ ics-workbench-lab4/cachesim/cache.c
@@ -57,7 +57,7 @@ void init_cache(int total_size_width, int associativity_width) {
   group_mask = mask_with_len(cache_group) << BLOCK_WIDTH;
   tag_mask   = ~mask_with_len(cache_group+BLOCK_WIDTH);
   //分配cache;
-  cache = (CACHE *)malloc(exp2(total_size_width - BLOCK_WIDTH));
+  cache = (struct CACHE *)malloc(exp2(total_size_width - BLOCK_WIDTH));
   assert(cache);
   for (size_t i = 0; i < exp2(total_size_width - BLOCK_WIDTH); i++)
   {
