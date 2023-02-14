12865db6471da29ca5451e1f992b95ab11bb1c80
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-14 08:27:24 +0800

    > compile NEMU ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:27:24 up 2 min,  1 user,  load average: 2.03, 1.27, 0.51

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -35,12 +35,12 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
 }
-#else
-#ifdef CACHE_CHECK
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
+#else
+#ifdef CACHE_CHECK
 
 
 #endif
