a645dc59029e5265d1f16f0e87e69b496b468d68
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-17 19:10:16 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:10:16 up 10:17,  1 user,  load average: 0.31, 0.38, 0.45

--- npc/playground/src/CACHE.scala
+++ npc/playground/src/CACHE.scala
@@ -151,7 +151,7 @@ class CpuCache extends Module with CacheParm{
             hitway := i.U
         }
     }
-    val BlockChoose = Wire(Vec(BlockNum+1,Bool()))
+    val BlockChoose = Wire(Vec(BlockNum,Bool()))
     for (i <- 0 until BlockNum) {
         BlockChoose(i) := (i.U>=writeblock) & (i.U<writeblock+(parm.REGWIDTH/DataWidth).U)
     }
