79fc875abcbfae938554b609a3544796314dabd6
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 21:14:26 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:14:26 up 11:32,  1 user,  load average: 1.07, 0.62, 0.41

--- npc/playground/src/CACHE.scala
+++ npc/playground/src/CACHE.scala
@@ -92,10 +92,10 @@ class CpuCache(Icache : Boolean = false) extends Module with CacheParm{
     //不同的block例化到同一个mem里是不合理的，因为会在一个周期内收集好几个Block的数据，这样的话，在同一个
     //会导致在同一个周期内，对不同的地址进行读写，这样子就不符实际，因此代码也会出现难以理解的行为
     //mem采用ReadMem实现的话，数据读出会有问题，目前还不知道为啥，暂时用寄存器实现
-    val mem = (Seq.fill(GroupNum*BlockNum)(SyncReadMem(AssoNum,UInt(DataWidth.W))))
+    val mem = (Seq.fill(GroupNum*BlockNum)(Mem(AssoNum,UInt(DataWidth.W))))
 
     //tag 实例化Assonum块 深度为Groupnum 的宽度为
-    val tag = Seq.fill(GroupNum)(SyncReadMem(AssoNum,UInt(TagWidth.W)))
+    val tag = Seq.fill(GroupNum)(Mem(AssoNum,UInt(TagWidth.W)))
     //容量小的用Reg实现//还是有点多，用Mem
     //val valid = Seq.fill(AssoNum)(SyncReadMem(GroupNum,Bool()))
     val valid = RegInit(VecInit(Seq.fill(AssoNum*GroupNum)((false.B))))
@@ -293,7 +293,7 @@ class CpuCache(Icache : Boolean = false) extends Module with CacheParm{
                                     for (i <- 0 until BlockNum){
                                         when(BlockChoose(i)){
                                             when(linemask(i)){
-                                                printf(p"i=${i} write=${Hexadecimal((lineData>>(i*DataWidth))(DataWidth-1,0))} \n")
+                                                //printf(p"i=${i} write=${Hexadecimal((lineData>>(i*DataWidth))(DataWidth-1,0))} \n")
                                                 mem(k*BlockNum+i).write(j.U,(lineData>>(i*DataWidth))(DataWidth-1,0))
                                             }
                                         }
