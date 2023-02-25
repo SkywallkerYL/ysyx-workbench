10f3054bbaa5b4e3a799c69449b04a74dc66f317
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-25 17:57:52 +0800

    > generate verilog ysyx_22050550 yangli Linux yangli-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:57:52 up  8:06,  1 user,  load average: 0.56, 0.36, 0.19

--- npc/playground/src/cpu.scala
+++ npc/playground/src/cpu.scala
@@ -179,7 +179,7 @@ class  RiscvCpu extends Module{
 */
     if(parm.DPI){
         val ebrdpi = Module(new ebreakDPI)
-        ebrdpi.io.a := Idu.io.ebreak 
+        ebrdpi.io.a := Wbu.io.debug.ebreak//Idu.io.ebreak 
         val pcdpi = Module(new pcDPI)
         pcdpi.io.pc := Wbu.io.debug.pc//Idu.io.pc_o
         pcdpi.io.dnpc := Wbu.io.debug.NextPc//NpcMux.io.NPC.npc
