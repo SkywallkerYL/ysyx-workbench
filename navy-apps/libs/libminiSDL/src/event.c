05a9d5ce56929eb3745076e2ba86d77212bad96f
yangli <577647772@qq.com>
2023-03-19 10:51:35 +0800

    dsa

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -41,7 +41,7 @@ int SDL_PollEvent(SDL_Event *ev) {
       if (strncmp(keybuf, keyname[i],len)==0&& strlen(keyname[i])==len)
       {
         keystates[i] = ev->type ==SDL_KEYDOWN?1:0;
-        //printf("%s %s\n",keybuf,keyname[i]);
+        printf("%s %s\n",keybuf,keyname[i]);
         ev->key.keysym.sym = i;
         break;
       }
@@ -83,7 +83,7 @@ int SDL_WaitEvent(SDL_Event *event) {
       if (strncmp(keybuf, keyname[i],len)==0 && strlen(keyname[i])==len)
       {
         keystates[i] = event->type ==SDL_KEYDOWN?1:0;
-        //printf("%s %s\n",keybuf,keyname[i]);
+        printf("%s %s\n",keybuf,keyname[i]);
         findflag = 1;
         event->key.keysym.sym = i;
         break;
