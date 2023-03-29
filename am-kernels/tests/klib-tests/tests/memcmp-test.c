0181e7897fee97c25bb9f5400f5b0cd7aa1cc62a
yangli <577647772@qq.com>
2023-03-29 12:20:41 +0800

    more test

--- am-kernels/tests/klib-tests/tests/memcmp-test.c
+++ am-kernels/tests/klib-tests/tests/memcmp-test.c
@@ -5,17 +5,29 @@ void test_memcmp(){
     char str1[] = "aacdef";
     char str2[] = "aavdef";
     int ret = memcmp(str1,str2,6);
+    int rett = strcmp(str1,str2);
+    int rettt = strncmp(str1,str2,6);
     //printf("%d\n",ret);
     //check_seq(0,l,1);
     assert(ret < 0);
+    assert(rett < 0);
+    assert(rettt < 0);
     char str3[] = "acwcdef";
     char str4[] = "aavdef";
     int ret1 = memcmp(str3,str4,6);
+    int rett1    = strcmp(str3,str4);
+    int rettt1   = strncmp(str3,str4,6);
     assert(ret1 > 0);
+        assert(rett1 >0);
+    assert(rettt1 > 0);
     char str5[] = "1234abc";
     char str6[] = "1234abc";
     int ret2 = memcmp(str5,str6,7);
+    int rett2 = strcmp(str5,str6);
+    int rettt2 = strncmp(str5,str6,7);
     assert(ret2 == 0);
+        assert(rett2 ==0);
+    assert(rettt2 ==0);
 }
 int main()
 {
