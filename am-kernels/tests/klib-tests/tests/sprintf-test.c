98e4dbb615af779037c29985e151692f8d6ca897
yangli <577647772@qq.com>
2023-03-29 13:17:53 +0800

    more test

--- am-kernels/tests/klib-tests/tests/sprintf-test.c
+++ am-kernels/tests/klib-tests/tests/sprintf-test.c
@@ -16,13 +16,17 @@ void test_sprintf(){
         /// 极限情况   会出错，   -int的极限值反转过来后 int已经装不下了，所以出错
         if(i==3) continue;
         sprintf(str,"%d",data[i]);
+        //sprintf(str,"%s",str11[i]);
         //printf("%d",data[i]);
-        printf("%s %d\n",str,data[i]);
-        printf("%s %s\n",str,str11[i]);
+        //printf("%s %d\n",str,data[i]);
+        //printf("%s %s\n",str,str11[i]);
         assert(strcmp(str,str11[i])==0);
         char str1[12];
         snprintf(str1,5,"%d",data[i]);
         printf("%s %d\n",str1,data[i]);
+        char str2[12];
+        sprintf(str2,"%s",str11[i]);
+        assert(strcmp(str2,str11[i])==0);
     }
     //0 126322567 2147483647 -2147483648 -2147483647 252645135 126322567
 }
