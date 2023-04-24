0c2c427b3aadffbffcfa98008c90e0a1d8060116
yangli <577647772@qq.com>
2023-04-24 20:44:30 +0800

    new projtest

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,7 +19,7 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
-
+//test
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
